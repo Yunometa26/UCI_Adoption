4d27b7ec4dad624d9a813d59e1df3ffba87972df
Yunometa26 <yunometa2021@gmail.com>
2025-07-11 15:13:14 +0530

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  base: './' // use './' to ensure compatibility with GitHub Pages or relative paths
-})
